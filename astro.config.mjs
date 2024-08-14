fd65f82d43f7b152e5ba290e89a763073e0d8608
Hrihorii Ilin <devopsick@pm.me>
2024-08-14 12:08:48 +0300

    add page redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 import cloudflare from '@astrojs/cloudflare'
 import markdoc from '@astrojs/markdoc'
+import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import keystatic from '@keystatic/astro'
 import tailwindcss from '@tailwindcss/vite'
+import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
+import rehypeExternalLinks from 'rehype-external-links'
+import { remarkReadingTime } from './src/lib/readTime'
+import { SITE } from './src/site.config.ts'
 
 export default defineConfig({
 	vite: {
+		build: {
+			cssMinify: 'lightningcss'
+		},
 		plugins: [tailwindcss()]
+	},
+	site: SITE,
+	prefetch: {
+		defaultStrategy: 'viewport',
+		prefetchAll: true
 	},
 	output: 'hybrid',
 	adapter: cloudflare({
@@ -15,5 +29,43 @@ export default defineConfig({
 			enabled: true
 		}
 	}),
-	integrations: [react(), markdoc(), keystatic()]
+	integrations: [
+		sitemap({
+			filter: (page) => page !== `${SITE}/keystatic`
+		}),
+		robotsTxt({
+			sitemap: 'https://www.honobunny.pages.dev/sitemap-0.xml',
+			host: 'devopsick.com'
+		}),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+				debug: false
+			}
+		}),
+		react(),
+		markdoc(),
+		keystatic()
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					content: {
+						type: 'text',
+						value: ' 🔗'
+					},
+					target: '_blank',
+					rel: ['nofollow', 'noreferrer']
+				}
+			]
+		],
+		remarkPlugins: [remarkReadingTime],
+		shikiConfig: {
+			theme: 'github-dark-dimmed',
+			wrap: true
+		},
+		gfm: true
+	}
 })
