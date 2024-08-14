40d8885710e4ae411c0e582b670a9e8df16efcbe
Hrihorii Ilin <devopsick@pm.me>
2024-08-14 11:46:20 +0300

    add shadcn tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import cloudflare from '@astrojs/cloudflare'
 import markdoc from '@astrojs/markdoc'
 import react from '@astrojs/react'
 import keystatic from '@keystatic/astro'
-import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
 	vite: {
